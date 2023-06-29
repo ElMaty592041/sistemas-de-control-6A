6f660cf0c7e7e1ce4759016ea167dbc6e0741be9
ElMaty592041 <107576879+ElMaty592041@users.noreply.github.com>
2023-06-29 16:41:32 -0300

    Add files via upload

---
+++ Modulo 4/Ejercicio 5.java
@@ -0,0 +1,15 @@
+public class Ejercicio 5 {
+    public static void main(String[] args) {
+        int puesto = 4; // Puesto a verificar (puedes cambiar este valor)
+
+        if (puesto == 1) {
+            System.out.println("El primero obtiene la medalla de oro.");
+        } else if (puesto == 2) {
+            System.out.println("El segundo obtiene la medalla de plata.");
+        } else if (puesto == 3) {
+            System.out.println("El tercero obtiene la medalla de bronce.");
+        } else {
+            System.out.println("Siga participando.");
+        }
+    }
+}
